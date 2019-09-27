1714793eea6988478d98ff8602ffbc318d2c8f5d
piccolo <kabiryusufbashir@gmail.com>
2019-09-26 21:00:36 -0700

    Reading about Arrays

--- index.php
+++ index.php
@@ -3,4 +3,19 @@
     var_dump($number);
     $number = 'abc';
     var_dump($number);
+
+    echo 'Arrays';
+    $books = [
+        'Introduction to Computer Applications'=>[
+            'author'=> 'Yusuf',
+            'status'=> true,
+            'cost'=> 3400
+        ],
+        'Web Development'=>[
+            'author'=>'Kabir Yusuf Bashir',
+            'status'=> false,
+            'cost'=> null
+        ]
+
+    ]
 ?>
