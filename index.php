733a9728dbc54d4f79aa7f42e134d7d1c1a0a353
piccolo <kabiryusufbashir@gmail.com>
2019-09-27 21:45:03 -0700

    learning foreach loop

--- index.php
+++ index.php
@@ -36,7 +36,48 @@
             <p>You are not looking for a book?</p>
         <?php 
         }
-        ?>
+        $books = [        
+                    [            
+                        'title' => 'To Kill A Mockingbird', 
+                        'author' => 'Harper Lee',    
+                        'available' => true,           
+                        'pages' => 336,            
+                        'isbn' => 9780061120084        
+                    ],        
+                    [            
+                        'title' => '1984',   
+                        'author' => 'George Orwell',      
+                        'available' => true,    
+                        'pages' => 267,     
+                        'isbn' => 9780547249643        
+                    ],        
+                    [            
+                        'title' => 'One Hundred Years Of Solitude',     
+                        'author' => 'Gabriel Garcia Marquez',      
+                        'available' => false,           
+                        'pages' => 457,          
+                        'isbn' => 9785267006323        
+                    ],    
+                ];
+           echo  '<ul>'; 
+                    
+                    foreach ($books as $book): 
+                    
+                    echo '       
+                        <li>            
+                            <i>
+                                '.$book['title'].'
+                            </i> 
+                                '.$book['author'];
+                                 
+                                if (!$book['available']):           
+                                    echo '<b>Not available</b>'; 
+                                endif;        
+                                    echo '</li>'; 
+                        endforeach;
+            echo   '</ul>';
+        ?>    
+                
     </body> 
 </html>
     
