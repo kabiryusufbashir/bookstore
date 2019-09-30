1f5145875f69b1eb3aaf275867ebfad4156af1af
piccolo <kabiryusufbashir@gmail.com>
2019-09-29 21:29:44 -0700

    I will be reading encapsulation tomorrow In sha Allahu

--- Book.php
+++ Book.php
@@ -1,6 +1,12 @@
 <?php
     class Book{
         
+        public $isbn;
+        public $title;
+        public $author;
+        public $page;
+        public $available;
+
         public function __construct(
             int $isbn, 
             string $title, 
@@ -33,11 +39,11 @@
         }
     }
 
-    $book = new Book("7867666565556", "Introduction to Computer Application", "Kabir Yusuf Bashir", "200",  12);
+    /* $book = new Book("7867666565556", "Introduction to Computer Application", "Kabir Yusuf Bashir", "200",  12);
   
     if($book->getCopy()){
         echo 'Here is your Copy of <b>'.$book->title.'</b> Sir <b>' .$book->available .' </b> Copies Left';
     }else{
         echo 'Please Call again Sir, We don\'t have a Copy Now ' .$book->available;
-    }
+    } */
 ?>
---
+++ init.php
@@ -0,0 +1,10 @@
+<?php
+    require_once __DIR__ . '/Book.php';
+    require_once __DIR__ . '/Customer.php';
+
+    $book1 = new Book("7867666565556", "Introduction to Computer Application", "Kabir Yusuf Bashir", "200",  12);
+    $book2 = new Book("8067666565556", "Introduction to Web Application", "Kabir Yusuf Bashir", "300",  122);
+  
+    $cust1 = new Customer(1, "Abba", "Jime Mustapha", "kkjjjja@fgffg.com")
+    $cust1 = new Customer(2, "Umar", "Grema", "kjhja@fgffg.com")
+?>
