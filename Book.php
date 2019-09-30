a4054e33dfc8d8d8be0ba7665eb6da05f7233f7a
piccolo <kabiryusufbashir@gmail.com>
2019-09-29 20:49:18 -0700

    Learning about constructors

--- Book.php
+++ Book.php
@@ -1,10 +1,19 @@
 <?php
     class Book{
-        public $isbn;
-        public $title;
-        public $author;
-        public $page;
-        public $available;
+        
+        public function __construct(
+            int $isbn, 
+            string $title, 
+            string $author, 
+            int $page, 
+            int $available=0){
+                $this->isbn = $isbn;
+                $this->title = $title;
+                $this->author = $author;
+                $this->page = $page;
+                $this->available = $available;
+        }
+        
 
         public function getPrintableTitle() : string{
             $result = '<i>' . $this->title .'</i> - ' . $this->author;
@@ -24,16 +33,11 @@
         }
     }
 
-    $book = new Book();
-    $book->isbn = "7867666565556";
-    $book->title = "Introduction to Computer Application";
-    $book->author = "Kabir Yusuf Bashir";
-    $book->pages = "200";
-    $book->available = true;
-
+    $book = new Book("7867666565556", "Introduction to Computer Application", "Kabir Yusuf Bashir", "200",  12);
+  
     if($book->getCopy()){
-        echo 'Here is your Copy Sir';
+        echo 'Here is your Copy of <b>'.$book->title.'</b> Sir <b>' .$book->available .' </b> Copies Left';
     }else{
-        echo 'Please Call again Sir, We don\'t have a Copy Now';
+        echo 'Please Call again Sir, We don\'t have a Copy Now ' .$book->available;
     }
 ?>
